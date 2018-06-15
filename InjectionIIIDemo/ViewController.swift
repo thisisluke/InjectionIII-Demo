19661e89c3c717d3093dde8de97c6a9fc4fd262b
thisisluke <luke.liu.au@outlook.com>
2018-06-15 20:58:17 +1000

    injection

--- InjectionIIIDemo/ViewController.swift
+++ InjectionIIIDemo/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var myLabel = UILabel()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Init")
+        myLabel.frame = CGRect(x: 100, y: 100, width: 200, height: 100)
+        myLabel.text = "init label"
         
+        self.view.addSubview(myLabel)
+        
+    }
+    
+    @objc func injected() {
+        myLabel.text = "injected label"
+        myLabel.center = CGPoint(x: 400, y: 200)
     }
 
     override func didReceiveMemoryWarning() {
